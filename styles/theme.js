577634d103eb75d7631107d91e43d69b9275a235
jon-vdb <jonvdberg8@gmail.com>
2022-04-03 12:52:36 +0700

    'Mui theme start'

---
+++ styles/theme.js
@@ -0,0 +1,19 @@
+import { pink, purple, red, black } from '@material-ui/core/colors'
+
+const theme = {
+  palette: {
+    primary: black,
+    secondary: pink
+  },
+
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightXLight: 300,
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700
+  }
+}
+
+export default theme
